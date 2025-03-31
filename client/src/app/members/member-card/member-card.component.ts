9258864d470ef120f0385078db68466807535a74
Jay Raja <jayraja15@hotmail.com>
2025-03-31 23:38:20 +0100

    End of Section 9

---
+++ client/src/app/members/member-card/member-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, input } from '@angular/core';
+import { Member } from '../../_models/member';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-member-card',
+  imports: [RouterLink],
+  templateUrl: './member-card.component.html',
+  styleUrl: './member-card.component.css'
+})
+export class MemberCardComponent {
+  member = input.required<Member>();
+ // @Input() memberI!: Member;
+
+}
